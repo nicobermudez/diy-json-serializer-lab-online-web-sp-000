e1f612f508c8162bbf6a7d223665da5afca4d25f
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-11 16:52:33 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,11 +3,6 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def data
-    product = Product.find(params[:id])
-    render json: ProductSerializer.serialize(post)
-  end
-
   def inventory
     product = Product.find(params[:id])
     render plain: product.inventory > 0 ? true : false
@@ -22,13 +17,18 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    Product.create(product_params)
+    redirect_to products_path
+  end
+
   def show
     @product = Product.find(params[:id])
   end
 
-  def create
-    Product.create(product_params)
-    redirect_to products_path
+  def data
+    product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
   end
 
   private
