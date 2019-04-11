f0239d5a55bb2e12df4849e522742bf984572ff7
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-11 16:42:21 +0000

    Automatically backed up by Learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -7,11 +7,8 @@ class ProductSerializer
 
     serialized_post += '"id": ' + product.id.to_s + ', '
     serialized_post += '"name": "' + product.name + '", '
+    
     serialized_post += '"description": "' + post.description + '" '
-
-    # the author association can also be represented in JSON
-    serialized_post += '"author": {'
-    serialized_post += '"name": "' + post.author.name + '"}'
 
     # and end with the close brace
     serialized_post += '}'
